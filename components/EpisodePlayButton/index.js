aac1bca8804788069792d09420a56d2e86243c66
Brian Lovin <briandlovin@gmail.com>
2018-11-07 10:31:04 -0800

    Switch to new react context consumer implementation

--- components/EpisodePlayButton/index.js
+++ components/EpisodePlayButton/index.js
@@ -15,50 +15,44 @@ type Props = {
 
 
 class EpisodePlayButton extends React.Component<Props> {
+  static contextType = GlobalPlayer
+
   render() {
     const { episode, } = this.props
+    const context = this.context
+
+    const shouldInitGlobalPlayer = context.trackQueue.length === 0
+    const isSameTrackAsGlobalPlayer = context.trackQueue.length > 0 && context.trackQueue[0].id === episode.id
+
+    let playAction
+    
+    if (shouldInitGlobalPlayer) {
+      playAction = () => context.addTrackToQueue(episode)
+    } else {
+
+      if (isSameTrackAsGlobalPlayer) {
+        if (context.isPlaying) {
+          playAction = () => context.pause()
+        } else {
+          playAction = () => context.play()
+        }
+      } else {
+        playAction = () => context.addTrackToQueue(episode)
+      }
+    }
     
     return (
-      <GlobalPlayer.Consumer>
-        {
-          context => {
-            const shouldInitGlobalPlayer = context.trackQueue.length === 0
-            const isSameTrackAsGlobalPlayer = context.trackQueue.length > 0 && context.trackQueue[0].id === episode.id
-
-            let playAction
-            
-            if (shouldInitGlobalPlayer) {
-              playAction = () => context.addTrackToQueue(episode)
-            } else {
-
-              if (isSameTrackAsGlobalPlayer) {
-                if (context.isPlaying) {
-                  playAction = () => context.pause()
-                } else {
-                  playAction = () => context.play()
-                }
-              } else {
-                playAction = () => context.addTrackToQueue(episode)
-              }
-            }
-
-            
-            return (
-              <MiniPlayBox onClick={playAction}>
-                <IconContainer>
-                  {
-                    context.isPlaying && isSameTrackAsGlobalPlayer
-                    ? <Icon glyph="pause" size={16} />
-                    : <Icon glyph="play" size={16} />
-                  }
-                </IconContainer>
-
-                <Label>{context.isPlaying ? 'Pause' : 'Play'}</Label>
-              </MiniPlayBox>
-            )
+      <MiniPlayBox onClick={playAction}>
+        <IconContainer>
+          {
+            context.isPlaying && isSameTrackAsGlobalPlayer
+            ? <Icon glyph="pause" size={16} />
+            : <Icon glyph="play" size={16} />
           }
-        }
-      </GlobalPlayer.Consumer>
+        </IconContainer>
+
+        <Label>{context.isPlaying ? 'Pause' : 'Play'}</Label>
+      </MiniPlayBox>
     )
   }
 }
--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,34 +5,31 @@ import Icon from '../Icon'
 import GlobalPlayer from '../GlobalPlayer/context'
 
 class Footer extends React.Component<{}> {
+  static contextType = GlobalPlayer
+
   render() {
+    const context = this.context
+
     return (
-      <GlobalPlayer.Consumer>
+      <Container addPlayerPadding={context.trackQueue.length > 0}>
+        <Icons>
+          <a href="https://twitter.com/brian_lovin" target="_blank" rel="noopener noreferrer">
+            <Icon glyph={'twitter'} />
+          </a>
+          
+          <a href="https://github.com/brianlovin/brian-lovin-next" target="_blank" rel="noopener noreferrer">
+            <Icon glyph={'github'} />
+          </a>
 
-        {
-          context => (
-            <Container addPlayerPadding={context.trackQueue.length > 0}>
-              <Icons>
-                <a href="https://twitter.com/brian_lovin" target="_blank" rel="noopener noreferrer">
-                  <Icon glyph={'twitter'} />
-                </a>
-                
-                <a href="https://github.com/brianlovin/brian-lovin-next" target="_blank" rel="noopener noreferrer">
-                  <Icon glyph={'github'} />
-                </a>
-
-                <a href="https://spectrum.chat/users/brian" target="_blank" rel="noopener noreferrer">
-                  <Icon glyph={'spectrum-solid'} />
-                </a>
-              </Icons>
-              
-              <Description>
-                Copyright whenever. This is <a href="https://github.com/brianlovin/brian-lovin-next" target="_blank" rel="noopener noreferrer">open source</a>.
-              </Description>
-            </Container>
-         )
-        }
-      </GlobalPlayer.Consumer>
+          <a href="https://spectrum.chat/users/brian" target="_blank" rel="noopener noreferrer">
+            <Icon glyph={'spectrum-solid'} />
+          </a>
+        </Icons>
+        
+        <Description>
+          Copyright whenever. This is <a href="https://github.com/brianlovin/brian-lovin-next" target="_blank" rel="noopener noreferrer">open source</a>.
+        </Description>
+      </Container>
     )
   }
 }
