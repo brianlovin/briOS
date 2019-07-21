6dbe44db19c066438881cf54eb5344be5412a399
Brian Lovin <briandlovin@gmail.com>
2019-07-21 11:34:52 -0600

    Fix flow

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ import { ServerStyleSheet } from 'styled-components';
 import { GA_TRACKING_ID } from '../lib/gtag';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: any) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
