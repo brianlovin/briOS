9f84f38f439c2debb2351d98edbeabd772481ae7
Brian Lovin <briandlovin@gmail.com>
2019-11-24 18:26:42 -0500

    Add fathom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,39 @@
  
 import * as React from 'react';
 import App from 'next/app';
+import Fathom from 'fathom-client'
 import { ATVImgStyles } from '../lib/atvimg/style';
 import { ATVScript } from '../lib/atvimg/script';
 import Providers from '../components/Providers';
+import Router from 'next/router'
+
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview()
+  ATVScript();
+})
+
+function FathomWrapper(props) {
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load();
+      Fathom.setSiteId('ONFMHEEY');
+      Fathom.trackPageview();
+    }
+    ATVScript();
+  }, [])
+  return <div {...props} />
+}
 
 class MyApp extends App {
-  componentDidMount() {
-    ATVScript();
-  }
-
-  componentDidUpdate() {
-    ATVScript();
-  }
-
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Providers>
-        <ATVImgStyles />
-        <Component {...pageProps} />
-      </Providers>
+      <FathomWrapper>
+        <Providers>
+          <ATVImgStyles />
+          <Component {...pageProps} />
+        </Providers>
+      </FathomWrapper>
     );
   }
 }
