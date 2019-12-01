d33c9b4eec55645bbcaf39c577096fa6b67d184e
Brian Lovin <briandlovin@gmail.com>
2019-12-01 15:41:00 -0500

    Fetch latest episode only when player is visible

--- src/components/DesignDetailsPlayer/index.tsx
+++ src/components/DesignDetailsPlayer/index.tsx
@@ -1,70 +1,68 @@
 import React from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 import { Card, ContentContainer } from './style'
 import { PlayerFooter } from './components'
 import LoadingSpinner from '../LoadingSpinner';
 
-class LatestEpisode extends React.Component {
-  state = {
-    episode: null,
-    loading: true
-  };
+export default function LatestEpisode() {
+  const [episode, setEpisode] = React.useState(null)
+  const [loading, setLoading] = React.useState(false)
+  const [visible, setVisible] = React.useState(false)
 
-  async componentDidMount() {
+  async function fetchEpisode() {
+    setLoading(true)
     return await fetch('https://spec.fm/api/podcasts/1034/episodes')
       .then(res => {
         return res.json();
       })
       .then(res => {
         const episode = res.find(ep => !!ep.published);
-        return this.setState({ loading: false, episode });
+        setLoading(false)
+        setEpisode(episode)
       })
-      .catch(() => this.setState({ loading: false }));
+      .catch(() => {
+        setLoading(false)
+      });
   }
 
-  render() {
-    const { episode, loading } = this.state;
-
-    if (loading) {
-      return (
-        <React.Fragment>
+  if (!episode || loading) {
+    return (
+      <React.Fragment>
+        <VisibilitySensor partialVisibility onChange={(visible) => visible && fetchEpisode()}>
           <Card data-cy="design-details-player">
             <ContentContainer>
               <LoadingSpinner />
             </ContentContainer>
           </Card>
-
-          <PlayerFooter />
-        </React.Fragment>
-      );
-    }
-
-    if (!episode) return null;
-
-    const { sharing_url } = episode;
-    const [, id] = sharing_url.split('s/');
-
-    if (!id) return null;
-
-    return (
-      <React.Fragment>
-        <Card data-cy="design-details-player">
-          <ContentContainer>
-            <iframe
-              frameBorder="0"
-              height="200px"
-              scrolling="no"
-              seamless
-              src={`https://embed.simplecast.com/${id}?color=f5f5f5`}
-              width="100%"
-              data-cy="latest-episode"
-            />
-          </ContentContainer>
-        </Card>
+        </VisibilitySensor>
 
         <PlayerFooter />
       </React.Fragment>
     );
   }
-}
 
-export default LatestEpisode;
+  const { sharing_url } = episode;
+  const [, id] = sharing_url.split('s/');
+
+  if (!id) return null;
+
+  return (
+    <React.Fragment>
+      <Card data-cy="design-details-player">
+        <ContentContainer>
+          <iframe
+            frameBorder="0"
+            height="200px"
+            scrolling="no"
+            seamless
+            src={`https://embed.simplecast.com/${id}?color=f5f5f5`}
+            width="100%"
+            data-cy="latest-episode"
+          />
+        </ContentContainer>
+      </Card>
+
+      <PlayerFooter />
+    </React.Fragment>
+  );
+}
