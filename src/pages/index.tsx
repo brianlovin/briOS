137a0d450770ff75e14e2dc4833d2209b9aa55c6
Brian Lovin <briandlovin@gmail.com>
2019-12-01 15:33:54 -0500

    Fix Link child

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
         <SectionHeading style={{ marginTop: '48px'}}>
           <LargeHeading>Brian Lovin</LargeHeading>
           <Subheading style={{ marginTop: '24px' }}>I'm a product designer living in New York City, currently building native mobile apps at GitHub.</Subheading>
-          <Subheading><Link href="/about">More about me</Link> &rarr;</Subheading>
+          <Subheading><Link href="/about"><a>More about me</a></Link> &rarr;</Subheading>
         </SectionHeading>
       </ContentContainer>
 
