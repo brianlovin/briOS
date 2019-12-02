a529a0207031e8665905df887f746547e654be1b
Brian Lovin <briandlovin@gmail.com>
2019-12-01 21:26:58 -0500

    Don't load design details card videos until visible

--- src/components/DesignDetailsCard/index.tsx
+++ src/components/DesignDetailsCard/index.tsx
@@ -12,6 +12,7 @@ import {
   Arrow,
   Circle,
 } from './style';
+import ReactVisibilitySensor from 'react-visibility-sensor';
 
 type Props = {
   post: DesignDetailsPost,
@@ -20,6 +21,7 @@ type Props = {
 
 export default function DesignDetailsCard(props: Props) {
   const videoEl = React.useRef(null);
+  const [isVisible, setIsVisible] = React.useState(false)
   const {
     post: { title, slug, details, tint },
   } = props;
@@ -37,31 +39,38 @@ export default function DesignDetailsCard(props: Props) {
   }
 
   return (
-    <Link href="/design-details/[slug]" as={`/design-details/${slug}`}>
-      <a>
-        <Container onMouseEnter={play} onMouseLeave={pause}>
-          <ImageContainer>
-            <Icon alt={'Design Details'} src={src} />
-          </ImageContainer>
-          <CardContent>
-            <Title>{title}</Title>
-            <DetailsCount>{details.length} details</DetailsCount>
-          </CardContent>
-          <Circle color={tint} />
-          <Arrow>
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h13M12 5l7 7-7 7"/></svg>
-          </Arrow>
-          <VideoPlayer 
-            playsInline
-            muted
-            ref={videoEl}
-            loop
-            autoPlay={false}
-            preload="metadata"
-            src={`${videosrc}#t=0.1`}
-          / >
-        </Container>
-      </a>
-    </Link>
+    <ReactVisibilitySensor
+      partialVisibility
+      onChange={(visible) => visible && setIsVisible(true)}
+    >
+      <Link href="/design-details/[slug]" as={`/design-details/${slug}`}>
+        <a>
+          <Container onMouseEnter={play} onMouseLeave={pause}>
+            <ImageContainer>
+              <Icon alt={'Design Details'} src={src} />
+            </ImageContainer>
+            <CardContent>
+              <Title>{title}</Title>
+              <DetailsCount>{details.length} details</DetailsCount>
+            </CardContent>
+            <Circle color={tint} />
+            <Arrow>
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h13M12 5l7 7-7 7"/></svg>
+            </Arrow>
+            {isVisible &&
+              <VideoPlayer 
+                playsInline
+                muted
+                ref={videoEl}
+                loop
+                autoPlay={false}
+                preload="metadata"
+                src={`${videosrc}#t=0.1`}
+              />
+            }
+          </Container>
+        </a>
+      </Link>
+    </ReactVisibilitySensor>
   );
 }
