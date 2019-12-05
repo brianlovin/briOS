4d2415eee3de94d5ec87b7b2480f215ae1198633
Brian Lovin <briandlovin@gmail.com>
2019-12-05 09:43:45 -0500

    Blog post improvements, syntax highlighting

--- src/pages/overthought/[slug].tsx
+++ src/pages/overthought/[slug].tsx
@@ -2,14 +2,17 @@
 import * as React from 'react';
 import Link from 'next/link';
 import useSWR from 'swr'
+import unified from 'unified'
+import parse from 'rehype-parse'
+import rehype2remark from 'rehype-remark'
+import stringify from 'remark-stringify'
 import { BlogPost } from '../../types';
 import { getPostBySlug } from '../../data/ghost'
 import Page, { ContentContainer, SectionHeading } from '../../components/Page';
-import { H1, Larr, Subheading } from '../../components/Typography'
+import { H1, Larr, Subheading, A } from '../../components/Typography'
 import { FeaturedImage, ReadingTime } from '../../components/OverthoughtPreviewCard/style'
 import Markdown from '../../components/Markdown';
 import PostShareButtons from '../../components/PostShareButtons';
-import Home from '..';
 
 interface Props {
   post: BlogPost;
@@ -20,8 +23,30 @@ export function OverthoughtPost(props: Props) {
   const initialData = props.post
   const { data: post } = useSWR(`${props.slug}`, getPostBySlug, { initialData })
 
-  if (!post) return <Home />
-  
+  if (!post) return (
+    <Page withHeader>
+      <ContentContainer>
+        <SectionHeading>
+          
+          <H1 style={{ marginTop: 0 }}>Post not Found</H1>
+
+          <Link href={'/overthought'}>
+            <A>
+              <Subheading><Larr /> Back to Overthought</Subheading>
+            </A>
+          </Link>
+        </SectionHeading>
+      </ContentContainer>
+    </Page>
+  )
+
+  const md = unified()
+    .use(parse)
+    .use(rehype2remark)
+    .use(stringify)
+    .processSync(post.html)
+    .toString()
+
   return (
     <Page withHeader>
       <ContentContainer>
@@ -35,12 +60,12 @@ export function OverthoughtPost(props: Props) {
           <div style={{ paddingBottom: '64px' }} />
 
           {post.feature_image && <FeaturedImage src={`https://overthought.ghost.io${post.og_image}`} />}
-          <H1>{post.title}</H1>
+          <H1 style={{ marginTop: 0 }}>{post.title}</H1>
           <ReadingTime>{post.reading_time} minute read</ReadingTime>
           <div style={{ padding: '16px '}} />
         </SectionHeading>
-        <Markdown escapeHtml={false}>
-          {post.html}
+        <Markdown>
+          {md}
         </Markdown>
 
         <SectionHeading>
