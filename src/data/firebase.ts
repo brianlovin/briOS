8e58ad0c9b2382b87bbb5beffcaeb2843a724ae2
Brian Lovin <brianlovin@github.com>
2020-01-02 00:17:00 -0700

    Test another import method

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -1,12 +1,11 @@
 import admin from 'firebase-admin'
-import serviceAccount from '~/data/service-account'
 
 const { apps } = admin
 
 if (!apps.length) {
   admin.initializeApp({
     // @ts-ignore
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(require('./service-account'))
   });
 }
 
