78fcc613ef3e7eec906353e2077faaa328fbc582
Brian Lovin <brianlovin@github.com>
2020-01-02 09:15:37 -0700

    Change secrets strategy

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -2,8 +2,19 @@ import admin from 'firebase-admin'
 
 const { apps } = admin
 
-let creds = JSON.parse(process.env.FIREBASE_CREDENTIALS, null)
-creds.private_key = process.env.FIREBASE_PRIVATE_KEY
+const creds = {
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+}
+
 
 if (!apps.length) {
   admin.initializeApp({
