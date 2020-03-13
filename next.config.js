5f5e3db1423fcd307d3eca9dfbbb075d9b034089
Brian Lovin <briandlovin@gmail.com>
2020-03-13 09:15:30 -0700

    Move secrets to build stage

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
     granularChunks: true,
     modern: true
   },
+  env: {
+    FATHOM_SITE_ID: process.env.FATHOM_SITE_ID,
+    GHOST_API_KEY: process.env.GHOST_API_KEY
+  },
   webpack: config => {
     config.resolve.alias['~'] = path.resolve('./src');
     return config;
