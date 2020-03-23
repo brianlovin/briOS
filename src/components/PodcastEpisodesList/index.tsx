995a49cb38e8612ec46d04bf94d4e95b3d9c8e4d
Brian Lovin <briandlovin@gmail.com>
2020-03-22 21:45:34 -0700

    Don't slice podcast episodes, we truncate on the server

--- src/components/PodcastEpisodesList/index.tsx
+++ src/components/PodcastEpisodesList/index.tsx
@@ -4,23 +4,9 @@ import LoadingSpinner from '../LoadingSpinner';
 import defaultTheme from '../Theme';
 
 export default function PodcastEpisodesList({ episodes }) {
-  if (!episodes) {
-    return (
-      <div style={{ 
-        padding: `${defaultTheme.space[8]}`,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-      }}>
-        <LoadingSpinner />
-      </div>
-    )
-  }
-
   return (
     <Ul style={{ listStyleType: 'none', marginLeft: 0 }}>
-      {episodes.slice(0, 4).map(ep => {
+      {episodes.map(ep => {
           return (
             <Li key={ep.id}>
               <a target="_blank" rel="noopener noreferrer" href={`https://spec.fm/podcasts/design-details/${ep.id}`}>
