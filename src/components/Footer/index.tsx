8e4c311b2e37c353e2c9fd999296d06b43f39b38
Logan McAnsh <logan@mcan.sh>
2020-03-23 11:18:53 -0400

    Revert "fix: add passHref prop to next/link when using a custom anchor tag"
    This reverts commit 09051fbc3986f67cf7b1cd775ca7c80978bb666e.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,38 +1,32 @@
-import React from "react";
-import Link from "next/link";
-import { A } from "~/components/Typography";
-import { Grid } from "./style";
+import React from 'react'
+import Link from 'next/link'
+import { A } from '~/components/Typography'
+import { Grid } from './style'
 
 export function Footer() {
   return (
     <Grid>
-      <Link href="/" as="/" passHref>
+      <Link href="/" as="/">
         <A>Home</A>
       </Link>
 
-      <Link href="/about" as="/about" passHref>
+      <Link href="/about" as="/about">
         <A>About</A>
       </Link>
 
-      <Link href="/overthought" as="/overthought" passHref>
+      <Link href="/overthought" as="/overthought">
         <A>Overthought</A>
       </Link>
 
-      <Link href="/design-details" as="/design-details" passHref>
+      <Link href="/design-details" as="/design-details">
         <A>App Dissection</A>
       </Link>
 
-      <Link href="/bookmarks" as="/bookmarks" passHref>
+      <Link href="/bookmarks" as="/bookmarks">
         <A>Bookmarks</A>
       </Link>
 
-      <A
-        href="https://twitter.com/brian_lovin"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        @brian_lovin
-      </A>
+      <A href="https://twitter.com/brian_lovin" target="_blank" rel="noopener noreferrer">@brian_lovin</A>
     </Grid>
-  );
+  )
 }
