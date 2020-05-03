5dd4b64f2b1f1a874bd17c54c80cbdce37abea81
Brian Lovin <briandlovin@gmail.com>
2020-05-03 10:35:45 -0700

    Add resolver definitions to server

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -2,6 +2,13 @@ import { getBookmarks } from '~/graphql/resolvers/bookmarks'
 import { getPosts, getPost } from '~/graphql/resolvers/overthought'
 import { getEpisodes } from '~/graphql/resolvers/podcast'
 import { getRepos } from '~/graphql/resolvers/repos'
+import { isMe } from '~/graphql/resolvers/isMe'
+import { login, logout } from '~/graphql/resolvers/mutations/auth'
+import {
+  addBookmark,
+  editBookmark,
+  deleteBookmark,
+} from '~/graphql/resolvers/mutations/bookmarks'
 
 export default {
   Query: {
@@ -10,5 +17,13 @@ export default {
     posts: getPosts,
     post: getPost,
     repos: getRepos,
+    isMe: isMe,
+  },
+  Mutation: {
+    login: login,
+    logout: logout,
+    addBookmark,
+    editBookmark,
+    deleteBookmark,
   },
 }
