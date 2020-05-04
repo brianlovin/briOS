dd2f4fbfb11d576f792de8c58bcf98165c9fded4
Brian Lovin <briandlovin@gmail.com>
2020-05-03 22:34:16 -0700

    Remove unneeded apple-touch-icon sizes

--- src/components/Providers/SEO.tsx
+++ src/components/Providers/SEO.tsx
@@ -34,46 +34,6 @@ export default function SEO() {
       <Head>
         <link
           rel="apple-touch-icon"
-          sizes="57x57"
-          href="/static/meta/apple-icon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/static/meta/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/static/meta/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/static/meta/apple-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/static/meta/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/static/meta/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/static/meta/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/static/meta/apple-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
           sizes="180x180"
           href="/static/meta/apple-icon-180x180.png"
         />
