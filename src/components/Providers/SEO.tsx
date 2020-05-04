020fc7e47377da33c58ebee002ede0b71654f280
Brian Lovin <briandlovin@gmail.com>
2020-05-03 23:36:33 -0700

    Simplify meta tag, dynamic favicon

--- src/components/Providers/SEO.tsx
+++ src/components/Providers/SEO.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
-import theme from '~/components/Theme'
 
 const SeoConfig = {
   title: 'Brian Lovin',
@@ -28,51 +28,30 @@ const SeoConfig = {
 }
 
 export default function SEO() {
+  const router = useRouter()
+
+  let emoji = '👾'
+  if (router.route.indexOf('/about') === 0) emoji = '👋'
+  if (router.route.indexOf('/overthought') === 0) emoji = '🤔'
+  if (router.route.indexOf('/design-details') === 0) emoji = '✨'
+  if (router.route.indexOf('/bookmarks') === 0) emoji = '📖'
+
   return (
     <React.Fragment>
       <DefaultSeo {...SeoConfig} />
       <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/static/meta/apple-icon-180x180.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/static/meta/android-icon-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/static/meta/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/static/meta/favicon-96x96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/static/meta/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/static/meta/site.webmanifest" />
-        <meta
-          name="msapplication-TileColor"
-          content={theme.colors['accent-blue'].light}
-        />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content={theme.colors['accent-blue'].light} />
+        <meta name="theme-color" content={'#fefefe'} />
+        <link rel="apple-touch-icon" href="/static/meta/apple-touch-icon.png" />
         <link
           rel="mask-icon"
-          href="/static/meta/website_icon.svg"
-          color={theme.colors['text-primary'].light}
-        ></link>
+          href="/static/meta/mask-icon.svg"
+          color={'#050505'}
+        />
+        <link rel="manifest" href="/static/meta/manifest.json" />
+        <link
+          rel="icon"
+          href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`}
+        />
       </Head>
     </React.Fragment>
   )
