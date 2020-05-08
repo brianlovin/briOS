159d2a4ccf90d563c338888cd86a417ec9bdaeb6
Brian Lovin <briandlovin@gmail.com>
2020-05-08 16:21:19 -0700

    Add code back, to be sure

--- src/components/Providers/Fathom.tsx
+++ src/components/Providers/Fathom.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react'
 
 export default function Fathom() {
-  // React.useEffect(() => {
-  //   const tracker = window.document.createElement('script')
-  //   const firstScript = window.document.getElementsByTagName('script')[0]
-  //   tracker.defer = true
-  //   tracker.setAttribute('site', process.env.FATHOM_SITE_ID)
-  //   tracker.setAttribute('spa', 'auto')
-  //   tracker.setAttribute('excluded-domains', 'localhost')
-  //   tracker.setAttribute('included-domains', 'brianlovin.com')
-  //   tracker.src = process.env.FATHOM_CUSTOM_URL
-  //   firstScript.parentNode.insertBefore(tracker, firstScript)
-  // }, [])
+  React.useEffect(() => {
+    const tracker = window.document.createElement('script')
+    const firstScript = window.document.getElementsByTagName('script')[0]
+    tracker.defer = true
+    tracker.setAttribute('site', process.env.FATHOM_SITE_ID)
+    tracker.setAttribute('spa', 'auto')
+    tracker.setAttribute('excluded-domains', 'localhost')
+    tracker.setAttribute('included-domains', 'brianlovin.com')
+    tracker.src = process.env.FATHOM_CUSTOM_URL
+    firstScript.parentNode.insertBefore(tracker, firstScript)
+  }, [])
 
   return null
 }
