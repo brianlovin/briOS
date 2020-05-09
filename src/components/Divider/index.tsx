6660f2e99ed7ccccf466b3fffc85c2920bfa3828
Brian Lovin <briandlovin@gmail.com>
2020-05-09 10:43:31 -0700

    Add simple divider component

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,11 @@
+export default function Divider() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '1px',
+        background: 'var(--border-primary)',
+      }}
+    />
+  )
+}
