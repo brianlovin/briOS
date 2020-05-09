835e2f7f4ade99f94e34449cba034334bb866acb
Brian Lovin <briandlovin@gmail.com>
2020-05-09 10:42:45 -0700

    Add generic input and textarea components

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -1,19 +1,6 @@
-import styled from 'styled-components'
-import theme from '~/components/Theme'
+import * as React from 'react'
+import Input from '~/components/Input'
 
-export default styled.textarea`
-  padding: ${theme.space[3]};
-  border: 1px solid var(--border-primary);
-  color: var(--text-primary);
-  border-radius: ${theme.space[2]};
-  font-size: 16px;
-  background: var(--bg-primary);
-  resize: vertical;
-  width: 100%;
-  line-height: ${theme.lineHeights.body};
-
-  &:focus {
-    border: 1px solid var(--accent-blue);
-    background: var(--bg-secondary);
-  }
-`
+export default function Textarea(props) {
+  return <Input as="textarea" {...props} />
+}
