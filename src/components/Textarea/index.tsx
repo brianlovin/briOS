79c28487a26ed3d29ab1d6ad9b7c9b0c86b3b413
Brian Lovin <briandlovin@gmail.com>
2020-05-09 20:36:00 -0700

    Resize textarea while typing

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -2,5 +2,14 @@ import * as React from 'react'
 import Input from '~/components/Input'
 
 export default function Textarea(props) {
-  return <Input as="textarea" {...props} />
+  const textInput = React.useRef(null)
+
+  function handleKeyDown(e) {
+    textInput.current.style.height = 'inherit'
+    textInput.current.style.height = `${e.target.scrollHeight}px`
+  }
+
+  return (
+    <Input as="textarea" onKeyDown={handleKeyDown} ref={textInput} {...props} />
+  )
 }
