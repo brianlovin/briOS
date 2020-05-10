b498360dc003a7d6ada0bc0412e7b53184d12a00
Brian Lovin <briandlovin@gmail.com>
2020-05-10 09:26:22 -0700

    Fix textarea perf

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -2,14 +2,5 @@ import * as React from 'react'
 import Input from '~/components/Input'
 
 export default function Textarea(props) {
-  const textInput = React.useRef(null)
-
-  function handleKeyDown(e) {
-    textInput.current.style.height = 'inherit'
-    textInput.current.style.height = `${e.target.scrollHeight}px`
-  }
-
-  return (
-    <Input as="textarea" onKeyDown={handleKeyDown} ref={textInput} {...props} />
-  )
+  return <Input as="textarea" {...props} />
 }
