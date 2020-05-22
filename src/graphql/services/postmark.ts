9cb16d4b861b4a943c68138cd24c8a4951e1a11a
Brian Lovin <briandlovin@gmail.com>
2020-05-22 10:10:27 -0700

    Add postmark service helper

--- src/graphql/services/postmark.ts
+++ src/graphql/services/postmark.ts
@@ -1,8 +1,33 @@
 import * as postmark from 'postmark'
+import { HNPost } from '~/pages/hn'
 
 const client = new postmark.ServerClient(process.env.POSTMARK_CLIENT_ID)
 
-export function emailMe({ subject, body }) {
+interface HNDigestProps {
+  email: string
+  date: string
+  posts: HNPost[]
+  unsubscribe_url: string
+}
+
+export function sendHNDigest(props: HNDigestProps) {
+  const { email, ...rest } = props
+  return client.sendEmailWithTemplate({
+    From: 'hi@brianlovin.com',
+    To: email,
+    TemplateId: 18037634,
+    TemplateModel: {
+      ...rest,
+    },
+  })
+}
+
+interface EmailMeProps {
+  subject: string
+  body: string
+}
+
+export function emailMe({ subject, body }: EmailMeProps) {
   return client.sendEmail({
     From: 'hi@brianlovin.com',
     To: 'hi@brianlovin.com',
