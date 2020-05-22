5104f24ea05c745b8fb4e0cb1f6ca8bbd5fb88c7
Brian Lovin <briandlovin@gmail.com>
2020-05-22 11:41:14 -0700

    Post url will always be correct

--- src/components/HNPosts/PostListItem.tsx
+++ src/components/HNPosts/PostListItem.tsx
@@ -11,13 +11,9 @@ interface Props {
 export const PostListItem = React.memo((props: Props) => {
   const { post } = props
 
-  const cleanUrl = post.domain
-    ? `${post.url}?ref=brianlovin.com`
-    : `/hn/${post.url.split('=')[1]}`
-
   return (
     <Grid gap={4}>
-      <A href={cleanUrl} target="_blank" rel="noopener noreferrer">
+      <A href={post.url} target="_blank" rel="noopener noreferrer">
         {post.title}
       </A>
 
