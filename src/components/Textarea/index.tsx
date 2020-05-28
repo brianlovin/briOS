93a566e0420ccfc75f9ad2d1a8883a22b8138a73
Brian Lovin <briandlovin@gmail.com>
2020-05-28 14:01:50 -0600

    Fix vertical resizing on textarea

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
-import styled from 'styled-components'
 import Input from '~/components/Input'
 
-export default function Textarea(props) {
-  const Input = styled.textarea`
-    resize: vertical;
-  `
-  return <Input as="textarea" {...props} />
+interface Props {
+  style?: object
+}
+
+export default function Textarea({ style, ...rest }: Props) {
+  return (
+    <Input as="textarea" {...rest} style={{ resize: 'vertical', ...style }} />
+  )
 }
