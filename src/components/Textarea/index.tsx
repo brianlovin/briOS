4e38e9d446b2461898c62359bfea81f28cba2ada
Ankur Chauhan <ankur.chauhan@auth0.com>
2020-05-28 20:43:19 +0200

    Possible fix for AMA textarea resize

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import Input from '~/components/Input'
 
 export default function Textarea(props) {
+  const Input = styled.textarea`
+    resize: vertical;
+  `
   return <Input as="textarea" {...props} />
 }
