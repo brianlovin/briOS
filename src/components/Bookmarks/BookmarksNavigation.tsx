a1666f539fd8c172808ddba5cd489976ffe27e54
Brian Lovin <brianlovin@github.com>
2020-11-29 13:49:49 -0800

    Tailwind perf (#1194)
    * Fix button width on timeline entry
    
    * Tailwind perf tweaks and bookmarks nav polish

--- src/components/Bookmarks/BookmarksNavigation.tsx
+++ src/components/Bookmarks/BookmarksNavigation.tsx
@@ -12,14 +12,14 @@ export default function BookmarksNavigation() {
   }
 
   return (
-    <div className="flex items-center -ml-4 -mr-10 overflow-x-auto border-b border-gray-300 tabbed-navigation md:-ml-0 md:-mr-0 flex-nowrap dark:border-gray-700">
+    <div className="flex items-center -ml-4 -mr-10 overflow-x-auto border-b border-gray-300 dark:border-gray-700 md:justify-center tabbed-navigation md:-ml-0 md:-mr-0 flex-nowrap">
       <Link href="/bookmarks">
         <a
           className={`tab ${
             isActive(undefined) ? 'tab-active' : 'tab-inactive'
           }`}
         >
-          <span className="flex items-center px-5 py-5 space-x-2">
+          <span className="flex items-center px-5 py-3 space-x-2">
             <Bookmark size={16} />
             <span>All</span>
           </span>
@@ -32,7 +32,7 @@ export default function BookmarksNavigation() {
             isActive('reading') ? 'tab-active' : 'tab-inactive'
           }`}
         >
-          <span className="flex items-center px-5 py-5 space-x-2">
+          <span className="flex items-center px-5 py-3 space-x-2">
             <BookOpen size={16} />
             <span>Reading</span>
           </span>
@@ -45,7 +45,7 @@ export default function BookmarksNavigation() {
             isActive('portfolio') ? 'tab-active' : 'tab-inactive'
           }`}
         >
-          <span className="flex items-center px-5 py-5 space-x-2">
+          <span className="flex items-center px-5 py-3 space-x-2">
             <Star size={16} />
             <span>Portfolios</span>
           </span>
@@ -58,7 +58,7 @@ export default function BookmarksNavigation() {
             isActive('website') ? 'tab-active' : 'tab-inactive'
           }`}
         >
-          <span className="flex items-center px-5 py-5 space-x-2">
+          <span className="flex items-center px-5 py-3 space-x-2">
             <Compass size={16} />
             <span>Personal Sites</span>
           </span>
