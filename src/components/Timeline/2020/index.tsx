a0877f64968bd9d0117bfa3242021b65af4e6d0c
Brian Lovin <briandlovin@gmail.com>
2020-12-18 10:54:26 -0700

    Add dec 2020 timeline

--- src/components/Timeline/2020/index.tsx
+++ src/components/Timeline/2020/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { December } from './December'
 import { November } from './November'
 import { October } from './October'
 
 export function Year2020() {
   return (
     <>
+      <December />
       <November />
       <October />
     </>
