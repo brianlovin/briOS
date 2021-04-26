6664c0cdae35c0b00d84728fdbb0bb1d6cf0386b
Brian Lovin <briandlovin@gmail.com>
2021-04-25 17:09:41 -0700

    Use default _app custom page syntax

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import App from 'next/app'
 import Providers from '~/components/Providers'
 import '~/styles/tailwind.css'
 import 'tailwindcss/utilities.css'
@@ -7,26 +6,12 @@ import '~/styles/custom-styles.css'
 import '~/styles/syntax-highlighting.css'
 import '~/styles/prose-styles.css'
 
-class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
-  render() {
-    const { Component, pageProps } = this.props
-
-    return (
-      <Providers>
-        <Component {...pageProps} />
-      </Providers>
-    )
-  }
+function MyApp({ Component, pageProps }) {
+  return (
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
+  )
 }
 
 export default MyApp
