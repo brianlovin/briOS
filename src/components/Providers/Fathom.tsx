8e92ef1e6ee10d940019461cf24bef171bea19e0
Brian Lovin <briandlovin@gmail.com>
2021-04-27 09:50:57 -0700

    Fix fathom tracking

--- src/components/Providers/Fathom.tsx
+++ src/components/Providers/Fathom.tsx
@@ -5,11 +5,11 @@ export default function Fathom() {
     const tracker = window.document.createElement('script')
     const firstScript = window.document.getElementsByTagName('script')[0]
     tracker.defer = true
-    tracker.setAttribute('site', process.env.FATHOM_SITE_ID)
+    tracker.setAttribute('site', process.env.NEXT_PUBLIC_ATHOM_SITE_ID)
     tracker.setAttribute('spa', 'auto')
     tracker.setAttribute('excluded-domains', 'localhost,now.sh')
     tracker.setAttribute('included-domains', 'brianlovin.com')
-    tracker.src = process.env.FATHOM_CUSTOM_URL
+    tracker.src = process.env.NEXT_PUBLIC_FATHOM_CUSTOM_URL
     firstScript.parentNode.insertBefore(tracker, firstScript)
   }, [])
 
