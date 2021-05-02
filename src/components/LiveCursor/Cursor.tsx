aa45b33a323d06de86ca1f222f701872cc87cc9b
Brian Lovin <briandlovin@gmail.com>
2021-05-02 10:57:27 -0700

    Remove old cursor svg

--- src/components/LiveCursor/Cursor.tsx
+++ src/components/LiveCursor/Cursor.tsx
@@ -79,26 +79,6 @@ type CursorProps = {
 
 export function Cursor({ presence, connectionId }: CursorProps) {
   return (
-    // <svg
-    // style={{
-    //   transition: 'transform 0.5s cubic-bezier(.17,.93,.38,1)',
-    //   transform: `translateX(${
-    //     presence.cursor.x + SVG_CURSOR_OFFSET_X
-    //   }px) translateY(${presence.cursor.y + SVG_CURSOR_OFFSET_Y}px)`,
-    // }}
-    // className="absolute hidden md:inline"
-    //   width="24"
-    //   height="36"
-    //   viewBox="0 0 24 36"
-    //   fill="none"
-    //   xmlns="http://www.w3.org/2000/svg"
-    // >
-    //   <path
-    //     d="M5.65376 12.3673H5.46026L5.31717 12.4976L0.500002 16.8829L0.500002 1.19841L11.7841 12.3673H5.65376Z"
-    //     fill={COLORS[connectionId % COLORS.length]}
-    //   />
-    // </svg>
-
     <svg
       width="24"
       height="36"
