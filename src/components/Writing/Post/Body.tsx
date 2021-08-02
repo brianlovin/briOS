ab2ccda7599420bbd964ff8842a51bb57798c1fb
Brian Lovin <briandlovin@gmail.com>
2021-08-02 10:25:10 -0700

    Update packages

--- src/components/Writing/Post/Body.tsx
+++ src/components/Writing/Post/Body.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import unified from 'unified'
+import { unified } from 'unified'
 import parse from 'rehype-parse'
 import rehype2remark from 'rehype-remark'
 import stringify from 'remark-stringify'
--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -1,4 +1,4 @@
-import { makeExecutableSchema } from 'graphql-tools'
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import typeDefs from '../typeDefs'
 import resolvers from '../resolvers'
 
