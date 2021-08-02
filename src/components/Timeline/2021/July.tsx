c4337ab5e554fd98815a926875f4a5cf968312f8
Paul C. Owe <40435982+owecodes@users.noreply.github.com>
2021-08-02 02:25:03 -0400

    July updates

---
+++ src/components/Timeline/2021/July.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { GitCommit, GitMerge, Zap, Wind, Twitter, Camera } from 'react-feather'
+import { ButtonSet, DateEntry, Notes, TimelineEntry } from '../Entry'
+import { BlogPost } from '../BlogPost'
+
+export function July() {
+  return (
+    <>
+      <DateEntry title="July, 2021" />
+
+      <TimelineEntry
+        title="Defining Infrastructure as Code with Terraform"
+        timestamp="July 29, 2021"
+        Icon={Zap}
+        tint="purple"
+      >
+        <div className="flex flex-col overflow-hidden transition-shadow bg-white rounded-md shadow md:flex-row dark:bg-gray-900 hover:shadow-cardHover">
+          <div className="flex flex-col justify-start px-3 py-3 space-y-2 md:w-1/2">
+            <p className="flex-1 px-2 font-normal">
+              Infrastructure as Code (IaC) provisioning for the DataMaps
+              platform on Google Cloud Platform. This Terrafrom template defines
+              all networking, compute, and storage components for automating the
+              deployment of a loosely-coupled, and highly scalable geolocation
+              microservice
+            </p>
+            <span />
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/paulowe/mining-twitter"
+            >
+              <div className="btn">
+                <GitMerge size={16} />
+                <span>View Terrafrom configuration scripts</span>
+              </div>
+            </a>
+          </div>
+          <div className="hidden w-full md:w-1/2 md:inline-block">
+            <Image
+              width="419"
+              height="452"
+              layout="responsive"
+              src="/static/img/project/terraform.png"
+              alt="A gif of the GCP logo"
+            />
+          </div>
+        </div>
+      </TimelineEntry>
+      <TimelineEntry
+        title="TensorFlow on AI Platform for training and model serving "
+        timestamp="July 22, 2021"
+        Icon={Camera}
+      >
+        <Notes>
+          <p>
+            Implemented an end-to-end solution for building a Tensorflow
+            training application, running on a single worker instance in the
+            cloud and deploying the model to support requests for{' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/paulowe/training-data-analyst/tree/master/self-paced-labs/tfx/tfx-ai-platform"
+            >
+              online predictions on AI platform
+            </a>
+            .{' '}
+          </p>
+        </Notes>
+        <div className="grid grid-cols-2 grid-rows-1">
+          <Image
+            src="/static/img/project/gcp_badge1.png"
+            width="1072"
+            height="1010"
+            layout="responsive"
+            className="rounded"
+            alt="Photo from the competition, Toronto"
+          />
+          <Image
+            src="/static/img/project/gcp.gif"
+            width="300"
+            height="300"
+            layout="responsive"
+            className="rounded"
+            alt="Photo from the competition, Toronto"
+          />
+        </div>
+      </TimelineEntry>
+    </>
+  )
+}
--- src/components/Timeline/2021/index.tsx
+++ src/components/Timeline/2021/index.tsx
@@ -5,10 +5,12 @@ import { March } from './March'
 import { April } from './April'
 import { May } from './May'
 import { June } from './June'
+import { July } from './July'
 
 export function Year2021() {
   return (
     <>
+      <July />
       <June />
       <May />
       <April />
