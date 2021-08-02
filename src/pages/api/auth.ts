8a57d1aae87dc3ef45254698ff9f28203cb0ebac
Brian Lovin <briandlovin@gmail.com>
2021-08-01 22:28:41 -0700

    Use test key for liveblocks

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,6 +1,10 @@
 import { authorize } from '@liveblocks/node'
-const secret = process.env.LIVEBLOCKS_API_KEY
+import { IS_PROD } from '~/graphql/constants'
 export default async function auth(req, res) {
+  const secret = IS_PROD
+    ? process.env.LIVEBLOCKS_API_KEY
+    : process.env.LIVEBLOCKS_TEST_KEY
+
   const room = req.body.room
   const result = await authorize({ room, secret })
   return res.status(result.status).end(result.body)
