982b01ac52d41426da61e50d88481d62f5978de6
Brian Lovin <briandlovin@gmail.com>
2021-08-01 22:29:51 -0700

    Add constants for audio storage

--- src/graphql/constants/index.ts
+++ src/graphql/constants/index.ts
@@ -1,6 +1,6 @@
 import { baseUrl } from '~/config/seo'
 
-export const CLIENT_URL =
-  process.env.NODE_ENV === 'production' ? baseUrl : 'http://localhost:3000'
-
+export const IS_PROD = process.env.NODE_ENV === 'production'
+export const CLIENT_URL = IS_PROD ? baseUrl : 'http://localhost:3000'
+export const AUDIO_STORAGE_BUCKET = IS_PROD ? 'audio-ama' : 'audio-ama-dev'
 export const PAGINATION_AMOUNT = 16
