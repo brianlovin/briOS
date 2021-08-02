aa28501be69f14412ef49bd2d508f4322a05bf21
Brian Lovin <briandlovin@gmail.com>
2021-08-01 22:32:45 -0700

    Hidden audio player component to back custom UI

---
+++ src/components/AudioPlayer/HiddenAudioPlayer.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+interface Props {
+  src: string
+}
+
+const HiddenAudioPlayer = React.forwardRef<HTMLAudioElement, Props>(
+  ({ src }: Props, ref) => {
+    return (
+      <audio preload="metadata" ref={ref} controls={false} className="hidden">
+        <source src={src} type="audio/mp3" />
+      </audio>
+    )
+  }
+)
+
+export default HiddenAudioPlayer
