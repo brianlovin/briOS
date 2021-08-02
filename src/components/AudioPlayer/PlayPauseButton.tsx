a8668c99329bd416152d12296047e17b80a25ecf
Brian Lovin <briandlovin@gmail.com>
2021-08-02 05:30:41 -0700

    Polish play button styles

--- src/components/AudioPlayer/PlayPauseButton.tsx
+++ src/components/AudioPlayer/PlayPauseButton.tsx
@@ -4,7 +4,7 @@ export default function PlayPauseButton({ isPlaying, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="flex items-center justify-center flex-none w-12 h-12 bg-black border border-black rounded-md cursor-pointer dark:bg-gray-600 hover:bg-blue-500 dark:focus:border-gray-500 hover:border-blue-600 dark:border-gray-600 focus:ring-0 focus:outline-none"
+      className="flex items-center justify-center flex-none w-12 h-12 bg-black border border-black rounded-md cursor-pointer dark:bg-gray-600 hover:bg-blue-500 dark:focus:border-blue-400 dark:focus:bg-blue-500 dark:hover:border-blue-400 hover:border-blue-600 focus:border-blue-600 dark:border-gray-600 focus:bg-blue-500 focus:ring-0 focus:outline-none"
     >
       {!isPlaying ? (
         <svg
