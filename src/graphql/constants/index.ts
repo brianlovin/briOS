e43b62bc1db278e6cccd6577a790968159efdf95
Brian Lovin <briandlovin@gmail.com>
2021-08-04 20:39:30 -0700

    Reset constants

--- src/graphql/constants/index.ts
+++ src/graphql/constants/index.ts
@@ -4,5 +4,5 @@ export const IS_PROD = process.env.NODE_ENV === 'production'
 export const CLIENT_URL = IS_PROD ? baseUrl : 'http://localhost:3000'
 export const PAGINATION_AMOUNT = 16
 
-export const AUDIO_STORAGE_BUCKET = !IS_PROD ? 'audio-ama' : 'audio-ama-dev'
-export const AMA_QUESTIONS_COLLECTION = !IS_PROD ? 'questions' : 'questions-dev'
+export const AUDIO_STORAGE_BUCKET = IS_PROD ? 'audio-ama' : 'audio-ama-dev'
+export const AMA_QUESTIONS_COLLECTION = IS_PROD ? 'questions' : 'questions-dev'
