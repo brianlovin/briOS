3c2f49d9934fae253bfb38e006d99b5ca512ae30
Brian Lovin <briandlovin@gmail.com>
2021-09-26 09:44:31 -0400

    Subtle shadow for white on white cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
       },
       boxShadow: {
         xs: '0 1px 2px 0px rgba(0,0,0,0.03)',
+        subtle: '0 3px 20px rgba(0,0,0,0.04)',
         cardHover:
           '0 4px 4.1px rgba(0, 0, 0, 0.012),0 4.9px 5.8px rgba(0, 0, 0, 0.018),0 6.3px 8.4px rgba(0, 0, 0, 0.029),0 8.8px 12.9px rgba(0, 0, 0, 0.05),0 15px 23px rgba(0, 0, 0, 0.11)',
       },
