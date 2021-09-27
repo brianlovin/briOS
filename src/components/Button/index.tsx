06d79373a138a0c0c0f38d078eb970831c23a713
Brian Lovin <briandlovin@gmail.com>
2021-09-27 07:54:49 -0700

    Consistent button radius

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -47,7 +47,7 @@ export function SmallButton({ className, href, children, ...rest }: Props) {
 export function DeleteButton({ className, ...rest }: Props) {
   return (
     <button
-      className={`flex items-center rounded justify-center flex-none px-4 py-1.5 space-x-3 font-medium bg-white border border-gray-200 dark:border-red-500 dark:hover:border-red-500 disabled:opacity-50 dark:bg-red-500 dark:border-opacity-20 dark:bg-opacity-10 disabled:bg-opacity-0 text-red-500 hover:border-red-500 hover:text-white hover:bg-red-600 focus:border-red-900 focus:ring-0 focus:outline-none ${className}`}
+      className={`flex items-center rounded-md justify-center flex-none px-4 py-1.5 space-x-3 font-medium bg-white border border-gray-200 dark:border-red-500 dark:hover:border-red-500 disabled:opacity-50 dark:bg-red-500 dark:border-opacity-20 dark:bg-opacity-10 disabled:bg-opacity-0 text-red-500 hover:border-red-500 hover:text-white hover:bg-red-600 focus:border-red-900 focus:ring-0 focus:outline-none ${className}`}
       {...rest}
     />
   )
@@ -56,7 +56,7 @@ export function DeleteButton({ className, ...rest }: Props) {
 export function RecordingButton({ className, ...rest }: Props) {
   return (
     <button
-      className={`flex items-center rounded justify-center flex-none px-4 py-1.5 space-x-3 font-medium bg-green-500 border border-green-600 dark:border-green-500 dark:hover:border-green-500 disabled:opacity-50 dark:bg-green-500 dark:border-opacity-20 dark:bg-opacity-10 disabled:bg-opacity-0 text-white hover:bg-green-600 focus:border-green-900 focus:ring-0 focus:outline-none ${className}`}
+      className={`flex items-center rounded-md justify-center flex-none px-4 py-1.5 space-x-3 font-medium bg-green-500 border border-green-600 dark:border-green-500 dark:hover:border-green-500 disabled:opacity-50 dark:bg-green-500 dark:border-opacity-20 dark:bg-opacity-10 disabled:bg-opacity-0 text-white hover:bg-green-600 focus:border-green-900 focus:ring-0 focus:outline-none ${className}`}
       {...rest}
     />
   )
@@ -65,7 +65,7 @@ export function RecordingButton({ className, ...rest }: Props) {
 export function LinkButton({ className, ...rest }: Props) {
   return (
     <button
-      className={`flex items-center rounded justify-center flex-none px-4 py-1.5 space-x-3 font-medium bg-transparent disabled:opacity-50 dark:bg-opacity-10 disabled:bg-opacity-0 text-gray-800 dark:text-gray-300 dark:hover:text-gray-200 hover:text-black hover:bg-gray-200 dark:hover:bg-gray-800 focus:ring-0 focus:outline-none border border-transparent focus:border-gray-200 dark:focus:border-gray-800 ${className}`}
+      className={`flex items-center rounded-md justify-center flex-none px-4 py-1.5 space-x-3 font-medium bg-transparent disabled:opacity-50 dark:bg-opacity-10 disabled:bg-opacity-0 text-gray-800 dark:text-gray-300 dark:hover:text-gray-200 hover:text-black hover:bg-gray-200 dark:hover:bg-gray-800 focus:ring-0 focus:outline-none border border-transparent focus:border-gray-200 dark:focus:border-gray-800 ${className}`}
       {...rest}
     />
   )
