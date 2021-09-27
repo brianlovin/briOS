26be31787ddae9b8c5aa5046a96f6558c4a589f0
Brian Lovin <briandlovin@gmail.com>
2021-09-27 07:43:16 -0700

    Show current user and settings link in sidebar

--- src/components/Sidebar/UserFooter.tsx
+++ src/components/Sidebar/UserFooter.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 import { useUser } from '@auth0/nextjs-auth0'
 import LoadingSpinner from '~/components/LoadingSpinner'
+import { Settings } from 'react-feather'
 
 function Container({ children }) {
   return (
-    <div className="sticky bottom-0 z-10 flex items-center p-2 space-x-3 bg-white border-t border-gray-150 dark:bg-gray-900 dark:border-gray-800 bg-opacity-80 filter-blur dark:bg-opacity-60">
+    <div className="sticky bottom-0 z-10 flex items-center justify-between p-2 space-x-3 bg-white border-t border-gray-150 dark:bg-gray-900 dark:border-gray-800 bg-opacity-80 filter-blur dark:bg-opacity-60">
       {children}
     </div>
   )
@@ -39,12 +42,20 @@ export function UserFooter() {
   if (user) {
     return (
       <Container>
-        <a
-          className="w-full text-sm font-medium py-1.5 text-center rounded-md text-primary hover:bg-gray-200 dark:hover:bg-gray-700"
-          href="/api/auth/logout"
-        >
-          Log out
-        </a>
+        <div className="flex items-center flex-none">
+          <Image
+            src={user.picture}
+            width={24}
+            height={24}
+            layout="fixed"
+            className="rounded-full"
+          />
+        </div>
+        <Link href="/settings">
+          <a className="p-2 text-center rounded-md text-tertiary hover:bg-gray-200 dark:hover:bg-gray-700">
+            <Settings size={16} />
+          </a>
+        </Link>
       </Container>
     )
   }
