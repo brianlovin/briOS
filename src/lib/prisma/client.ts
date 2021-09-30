20ec12295a105abd24f9731c45b3889b17eb530b
Brian Lovin <briandlovin@gmail.com>
2021-09-29 23:12:30 -0700

    Disable prisma logging for now

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -4,10 +4,6 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
+export const prisma = global.prisma || new PrismaClient({})
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
