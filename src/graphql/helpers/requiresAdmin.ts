cb36e2c7f489fe402a692ffa80549066ff801cc3
Brian Lovin <briandlovin@gmail.com>
2021-09-29 23:14:21 -0700

    Use generated type for role check in requiresAdmin

--- src/graphql/helpers/requiresAdmin.ts
+++ src/graphql/helpers/requiresAdmin.ts
@@ -1,8 +1,12 @@
 import { AuthenticationError } from 'apollo-server-micro'
+import { UserRole } from '../types.generated'
 
 export function requiresAdmin(fn) {
   return function resolve(parent, args, context) {
-    if (context?.viewer?.role === 'ADMIN') return fn(parent, args, context)
+    if (context?.viewer?.role === UserRole.Admin) {
+      return fn(parent, args, context)
+    }
+
     throw new AuthenticationError('You can’t do that!')
   }
 }
