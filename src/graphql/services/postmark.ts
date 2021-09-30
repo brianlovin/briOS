acc52b2df9d185d80f7af1a79bc9e2086e7d4636
Brian Lovin <briandlovin@gmail.com>
2021-09-30 07:47:48 -0700

    Dont email me in dev mode

--- src/graphql/services/postmark.ts
+++ src/graphql/services/postmark.ts
@@ -1,6 +1,7 @@
 import * as postmark from 'postmark'
 import { baseEmail } from '~/config/seo'
 import { HNPost } from '~/pages/hn'
+import { IS_PROD } from '../constants'
 
 export const client = new postmark.ServerClient(process.env.POSTMARK_CLIENT_ID)
 
@@ -17,6 +18,15 @@ interface EmailMeProps {
 }
 
 export function emailMe({ subject, body }: EmailMeProps) {
+  if (!IS_PROD) {
+    return console.log('Sending Postmark email: ', {
+      From: baseEmail,
+      To: baseEmail,
+      Subject: subject,
+      TextBody: body,
+    })
+  }
+
   return client.sendEmail({
     From: baseEmail,
     To: baseEmail,
