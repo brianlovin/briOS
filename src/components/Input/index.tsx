212445d7c9fe664bf84903a63957c6ae9453298b
Brian Lovin <briandlovin@gmail.com>
2021-10-14 17:32:03 -0700

    Add autoresizing text area

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import TextareaAutosize from 'react-autosize-textarea'
 
 const styles =
   'w-full rounded-md focus:outline-none px-4 py-2 focus:border-gray-1000 text-primary border-gray-300 dark:border-gray-800 dark:focus:border-gray-600 bg-gray-1000 dark:bg-white bg-opacity-5 hover focus:ring-0'
@@ -7,8 +8,15 @@ export function Input(props) {
   return <input className={styles} {...props} />
 }
 
-export function Textarea(props) {
-  return <textarea className={`${styles} block`} {...props} />
+export function Textarea({ maxRows = 8, rows = 2, ...props }) {
+  return (
+    <TextareaAutosize
+      maxRows={maxRows}
+      rows={rows}
+      className={`${styles} block`}
+      {...props}
+    />
+  )
 }
 
 export function Select(props) {
