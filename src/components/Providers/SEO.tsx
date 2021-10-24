4757c46ea193f400ce345ee8b4174313b367ff7c
Brian Lovin <briandlovin@gmail.com>
2021-10-24 15:50:05 -0700

    Update favicon

--- src/components/Providers/SEO.tsx
+++ src/components/Providers/SEO.tsx
@@ -6,30 +6,20 @@ import * as React from 'react'
 import { baseUrl, defaultSEO } from '~/config/seo'
 
 export function SEO() {
-  const router = useRouter()
-
-  let emoji = '👾'
-  if (router.route.indexOf('/about') === 0) emoji = '👋'
-  if (router.route.indexOf('/writing') === 0) emoji = '🤔'
-  if (router.route.indexOf('/app-dissection') === 0) emoji = '✨'
-  if (router.route.indexOf('/bookmarks') === 0) emoji = '📖'
-
   return (
     <React.Fragment>
       <DefaultSeo {...defaultSEO} />
       <Head>
-        <meta name="theme-color" content={'#fefefe'} />
-        <link rel="apple-touch-icon" href="/static/meta/apple-touch-icon.png" />
-        <link
-          rel="mask-icon"
-          href="/static/meta/mask-icon.svg"
-          color={'#050505'}
-        />
-        <link rel="manifest" href="/static/meta/manifest.json" />
+        <link rel="icon" href="/static/favicon.ico" sizes="any" />
         <link
           rel="icon"
-          href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`}
+          href="/static/favicon.svg"
+          type="image/svg+xml"
+          sizes="any"
         />
+        <link rel="mask-icon" href="/static/meta/mask-icon.svg" />
+        <link rel="apple-touch-icon" href="/static/meta/apple-touch-icon.png" />
+        <link rel="manifest" href="/static/meta/manifest.webmanifest" />
         <link
           rel="alternate"
           type="application/rss+xml"
