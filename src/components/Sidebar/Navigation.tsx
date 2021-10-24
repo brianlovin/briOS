aa22cb9ea6fa73dfe81729eb6bebd2d31b66ebc6
Brian Lovin <briandlovin@gmail.com>
2021-10-24 12:06:27 -0700

    Fix invalid html nesting in sidebar list

--- src/components/Sidebar/Navigation.tsx
+++ src/components/Sidebar/Navigation.tsx
@@ -160,12 +160,12 @@ export function SidebarNavigation() {
       {links.map((link, i) => {
         if (typeof link === 'string') {
           return (
-            <p
+            <li
               key={i}
               className="px-2 pt-5 pb-2 text-xs font-semibold text-gray-1000 dark:text-white text-opacity-40"
             >
               {link}
-            </p>
+            </li>
           )
         }
 
