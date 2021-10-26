dd9ee3a97c393cebc8b8ba889deaa3528b790fba
Brian Lovin <briandlovin@gmail.com>
2021-10-26 07:29:31 -0700

    Always divide hn comments

--- src/components/HackerNews/PostComments.tsx
+++ src/components/HackerNews/PostComments.tsx
@@ -9,7 +9,7 @@ export function PostComments({ comments }) {
     <>
       <SyntaxHighlighter data={comments} />
       <div className="relative flex flex-col flex-1 pb-24 border-t dark:border-gray-800 border-gray-150">
-        <div className="flex flex-col flex-1 w-full max-w-3xl mx-auto space-y-8 divide-y divide-gray-150 md:space-y-12 xl:divide-y-0 dark:divide-gray-800">
+        <div className="flex flex-col flex-1 w-full max-w-3xl mx-auto space-y-8 divide-y divide-gray-150 md:space-y-12 dark:divide-gray-800">
           {comments &&
             comments.length > 0 &&
             comments.map((comment) => (
