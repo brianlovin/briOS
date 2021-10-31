f242c48a73fc83c85248ad514c42de922a4ab289
Brian Lovin <briandlovin@gmail.com>
2021-10-31 14:06:16 -0700

    Better mobile heights and safe insets

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,5 +48,6 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
+    require('tailwindcss-safe-area'),
   ],
 }
