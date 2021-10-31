a1c50b4ffd203122d47479cd0c33c0e950a6364f
Brian Lovin <briandlovin@gmail.com>
2021-10-30 22:57:11 -0700

    Don't return unpublished posts

--- src/graphql/resolvers/queries/posts/index.ts
+++ src/graphql/resolvers/queries/posts/index.ts
@@ -1,10 +1,11 @@
 import { Context } from '~/graphql/context'
-import { GetPostQueryVariables } from '~/graphql/types.generated'
+import { GetPostQueryVariables, UserRole } from '~/graphql/types.generated'
 
 export async function getPosts(_, __, ctx: Context) {
   const { prisma } = ctx
   return await prisma.post.findMany({
     orderBy: { publishedAt: 'desc' },
+    where: { publishedAt: { not: null } },
   })
 }
 
@@ -13,8 +14,13 @@ export async function getPost(
   { slug }: GetPostQueryVariables,
   ctx: Context
 ) {
-  const { prisma } = ctx
-  return await prisma.post.findUnique({
-    where: { slug },
-  })
+  const { prisma, viewer } = ctx
+  const isAdmin = viewer?.role === UserRole.Admin
+  const post = await prisma.post.findUnique({ where: { slug } })
+
+  if (!post?.publishedAt && !isAdmin) {
+    return null
+  }
+
+  return post
 }
