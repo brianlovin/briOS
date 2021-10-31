a361e5438dd1004145676e04cab5723e4b32b7e8
Brian Lovin <briandlovin@gmail.com>
2021-10-31 12:52:15 -0700

    Specify logout returnTo

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,5 +1,11 @@
-import { handleAuth, handleCallback, handleLogin } from '@auth0/nextjs-auth0'
+import {
+  handleAuth,
+  handleCallback,
+  handleLogin,
+  handleLogout,
+} from '@auth0/nextjs-auth0'
 
+import { CLIENT_URL } from '~/graphql/constants'
 import { afterCallback } from '~/lib/auth0/afterCallback'
 
 export default handleAuth({
@@ -7,6 +13,13 @@ export default handleAuth({
     try {
       await handleLogin(req, res, {
         returnTo: req.headers.referer,
+      })
+    } catch (error) {}
+  },
+  async logout(req, res) {
+    try {
+      await handleLogout(req, res, {
+        returnTo: CLIENT_URL,
       })
     } catch (error) {}
   },
