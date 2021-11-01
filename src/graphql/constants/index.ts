e0596af2ac778b5e4fe8587caecf6a7537672943
Brian Lovin <briandlovin@gmail.com>
2021-11-01 11:21:07 -0700

    Bump rate limit again temporarily

--- src/graphql/constants/index.ts
+++ src/graphql/constants/index.ts
@@ -4,7 +4,7 @@ export const IS_PROD = process.env.NODE_ENV === 'production'
 export const CLIENT_URL = IS_PROD ? baseUrl : 'http://localhost:3000'
 export const PAGINATION_AMOUNT = 24
 
-export const RATE_LIMIT_REQUEST_AMOUNT = 256
+export const RATE_LIMIT_REQUEST_AMOUNT = 1024
 export const RATE_LIMIT_REQUEST_DURATION = 10 * 1000 // 10 seconds
 
 export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
