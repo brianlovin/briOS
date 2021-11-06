6a23ef7f7b5a2b87e222b01bb39c70c2ec8b1422
Brian Lovin <briandlovin@gmail.com>
2021-11-06 10:00:46 -0700

    Reorder env variables

--- src/graphql/constants/index.ts
+++ src/graphql/constants/index.ts
@@ -1,21 +1,21 @@
 import { baseUrl } from '~/config/seo'
 
 export const IS_PROD = process.env.NODE_ENV === 'production'
-export const VERCEL_PROD = process.env.VERCEL_ENV === 'production'
-export const VERCEL_PREVIEW = process.env.VERCEL_ENV === 'preview'
+export const IS_DEV = process.env.NODE_ENV === 'development'
+export const IS_PREVIEW = process.env.VERCEL_ENV === 'preview'
 export const GRAPHCDN_PURGE_ENDPOINT = process.env.GRAPHCDN_PURGE_ENDPOINT
 export const CLIENT_URL = IS_PROD ? baseUrl : 'http://localhost:3000'
 
-export const GRAPHQL_ENDPOINT = VERCEL_PREVIEW
+export const GRAPHQL_ENDPOINT = IS_DEV
+  ? '/api/graphql'
+  : IS_PREVIEW
   ? `https://${process.env.VERCEL_URL}/api/graphql`
-  : IS_PROD
-  ? process.env.GRAPHQL_ENDPOINT
-  : '/api/graphql'
+  : process.env.GRAPHQL_ENDPOINT
 
 console.log({
   endpiont: GRAPHQL_ENDPOINT,
-  isPreview: VERCEL_PREVIEW,
-  isProd: VERCEL_PROD,
+  isPreview: IS_PREVIEW,
+  isProd: IS_PROD,
   env: process.env.GRAPHQL_ENDPOINT,
 })
 
