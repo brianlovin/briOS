896b24808bb61420e5d767691ec0c7d3f5c8ea7b
Brian Lovin <briandlovin@gmail.com>
2021-11-13 08:38:47 -0800

    Fallback avatar state

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -2,11 +2,15 @@ import Image from 'next/image'
 import * as React from 'react'
 
 export function Avatar({ user, src, ...props }) {
+  const [srcState, setSrcState] = React.useState(src)
   return (
     <Image
       alt={`${user.name || user.username}'s profile photo`}
-      src={src || '/static/img/fallback-avatar.png'}
+      src={srcState}
       {...props}
+      onError={() => {
+        setSrcState('/static/img/fallback-avatar.png')
+      }}
     />
   )
 }
