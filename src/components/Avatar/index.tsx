3270fba1c36af461cfb6fc178c6bdf4c4eb823ef
Brian Lovin <brianlovin@github.com>
2021-11-13 09:01:34 -0800

    Fallback avatar image (#1796)
    * Fallback avatar state
    
    * Add fallback on initial load

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -2,11 +2,16 @@ import Image from 'next/image'
 import * as React from 'react'
 
 export function Avatar({ user, src, ...props }) {
+  const fallbackUrl = '/static/img/fallback-avatar.png'
+  const [srcState, setSrcState] = React.useState(src || fallbackUrl)
   return (
     <Image
       alt={`${user.name || user.username}'s profile photo`}
-      src={src || '/static/img/fallback-avatar.png'}
+      src={srcState}
       {...props}
+      onError={() => {
+        setSrcState(fallbackUrl)
+      }}
     />
   )
 }
