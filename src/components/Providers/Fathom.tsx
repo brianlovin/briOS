cbb3294c8ed661e2ed9d57cae9004d90057212c6
Brian Lovin <briandlovin@gmail.com>
2021-11-14 09:02:27 -0800

    Use data attributes

--- src/components/Providers/Fathom.tsx
+++ src/components/Providers/Fathom.tsx
@@ -5,10 +5,10 @@ export function Fathom() {
     const tracker = window.document.createElement('script')
     const firstScript = window.document.getElementsByTagName('script')[0]
     tracker.defer = true
-    tracker.setAttribute('site', process.env.NEXT_PUBLIC_FATHOM_SITE_ID)
-    tracker.setAttribute('spa', 'auto')
-    tracker.setAttribute('excluded-domains', 'localhost,now.sh,vercel.app')
-    tracker.setAttribute('included-domains', 'brianlovin.com')
+    tracker.setAttribute('data-site', process.env.NEXT_PUBLIC_FATHOM_SITE_ID)
+    tracker.setAttribute('data-spa', 'auto')
+    tracker.setAttribute('data-excluded-domains', 'localhost,now.sh,vercel.app')
+    tracker.setAttribute('data-included-domains', 'brianlovin.com')
     tracker.src = process.env.NEXT_PUBLIC_FATHOM_CUSTOM_URL
     firstScript.parentNode.insertBefore(tracker, firstScript)
   }, [])
