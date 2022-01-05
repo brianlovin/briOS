bbda89345796ffd232cb403607f2a3fa9ccb02d0
Brian Lovin <briandlovin@gmail.com>
2022-01-05 07:22:37 -0800

    Fix typescript

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -105,7 +105,7 @@ export default {
           },
         ]
 
-      const [hn, newsletter] = await Promise.all([
+      const [hn] = await Promise.all([
         prisma.emailSubscription.findUnique({
           where: {
             emailAndType: {
