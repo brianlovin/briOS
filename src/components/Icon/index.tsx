b79050fa33101798b56462aadb24a5abf5582dd4
Brian Lovin <brianlovin@github.com>
2022-01-28 08:14:18 -0800

    Visual nits (#1948)
    * Fix crit icon fill
    
    * Fix code block colors

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -40,6 +40,7 @@ export function CritIcon() {
       height="14"
       viewBox="0 0 14 14"
       stroke="currentColor"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
