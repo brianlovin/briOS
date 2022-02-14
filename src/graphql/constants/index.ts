4609ab6100f234267f87c5178d6c144070bea360
Brian Lovin <briandlovin@gmail.com>
2022-02-13 16:15:07 -0800

    Add GraphCDN back

--- src/graphql/constants/index.ts
+++ src/graphql/constants/index.ts
@@ -14,7 +14,7 @@ export const GRAPHQL_ENDPOINT = IS_DEV
   ? '/api/graphql'
   : IS_PREVIEW
   ? `https://${PREVIEW_URL}/api/graphql`
-  : '/api/graphql'
+  : 'https://brios.brianlovin.com'
 
 export const PAGINATION_AMOUNT = 24
 
