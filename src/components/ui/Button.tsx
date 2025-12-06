3777dd4f47ea69c4f30522bafb37b4bb86ebf4f6
Brian Lovin <briandlovin@gmail.com>
2025-12-06 12:17:57 -0800

    prettier

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -43,7 +43,8 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
