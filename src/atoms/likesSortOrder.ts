5f8c7067ffbfecf998c5fc85382cfb1130d91730
Brian Lovin <blovin@makenotion.com>
2026-04-13 15:09:43 -0700

    Add sortable table headers

--- src/atoms/likesSortOrder.ts
+++ src/atoms/likesSortOrder.ts
@@ -1,13 +1,53 @@
 import { atomWithStorage } from "jotai/utils";
 
-export type LikesSortOrder = "none" | "desc" | "asc";
+export type SortDirection = "none" | "desc" | "asc";
 
-export const sitesLikesSortOrderAtom = atomWithStorage<LikesSortOrder>(
-  "sites-likes-sort-order",
-  "none",
+interface TableSortState<TColumn extends string> {
+  column: TColumn | null;
+  direction: SortDirection;
+}
+
+export type SitesTableSortColumn = "name" | "site" | "likes";
+export type StackTableSortColumn = "name" | "description" | "platforms" | "likes";
+
+export type SitesTableSortState = TableSortState<SitesTableSortColumn>;
+export type StackTableSortState = TableSortState<StackTableSortColumn>;
+
+const defaultSitesTableSort: SitesTableSortState = { column: null, direction: "none" };
+const defaultStackTableSort: StackTableSortState = { column: null, direction: "none" };
+
+export const sitesTableSortAtom = atomWithStorage<SitesTableSortState>(
+  "sites-table-sort",
+  defaultSitesTableSort,
 );
 
-export const stackLikesSortOrderAtom = atomWithStorage<LikesSortOrder>(
-  "stack-likes-sort-order",
-  "none",
+export const stackTableSortAtom = atomWithStorage<StackTableSortState>(
+  "stack-table-sort",
+  defaultStackTableSort,
 );
+
+export function getNextTableSort<TColumn extends string>(
+  currentSort: TableSortState<TColumn>,
+  column: TColumn,
+  defaultDirection: Exclude<SortDirection, "none">,
+): TableSortState<TColumn> {
+  if (currentSort.column !== column || currentSort.direction === "none") {
+    return { column, direction: defaultDirection };
+  }
+
+  if (currentSort.direction === defaultDirection) {
+    return {
+      column,
+      direction: defaultDirection === "asc" ? "desc" : "asc",
+    };
+  }
+
+  return { column: null, direction: "none" };
+}
+
+export function getTableSortDirection<TColumn extends string>(
+  currentSort: TableSortState<TColumn>,
+  column: TColumn,
+): SortDirection {
+  return currentSort.column === column ? currentSort.direction : "none";
+}
